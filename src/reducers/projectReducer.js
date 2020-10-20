0f1b51d2a2f759ba7324b9676d18b8612d0d8541
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2020-10-21 00:56:56 +0300

    added redux reducer

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -1,5 +1,13 @@
-const projectReducer = (state = {}, action) => {
+const initialState = {
+  project: [
+    {id: 1, title: 'Help to find stars', content: "Hello, I'm here, that because"},
+    {id: 2, title: 'The main instinct', content: "You find yourself on the sofa"},
+    {id: 2, title: 'In jazz man only', content: "Are you back star, Yeah"},
+  ]
+};
+
+const projectReducer = (state = initialState, action) => {
   return state;
-}
+};
 
 export default projectReducer;
